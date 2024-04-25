bb453baa6665b5726c84a43c43b6bcfac9e7deb4
Antonio Leskur <antonioleskur93@gmail.com>
2024-04-25 19:54:56 +0200

    save game

---
+++ formule.py
@@ -0,0 +1,5 @@
+import math
+from kordinate import *
+
+
+
--- test.py
+++ test.py
@@ -12,13 +12,12 @@ def dali_su_tocke_razlicite(a, b, c):
         return True
     else:
         return False
-
-
 class ProvjeraKuteva:
     def __init__(self, x_lista, y_lista):
         self.sve_x_kordinate = self.provjera_kuta(x_lista, 0)
         self.sve_y_kordinate = self.provjera_kuta(y_lista, 0)
-        self.kutevi_su_ok = False
+        self.kutrvi_su_ok = False
+        self.dali_su_kutevi_ok()
 
     def provjera_kuta(self, lista, rezultat):
         for kordinata in lista:
@@ -30,9 +29,25 @@ class ProvjeraKuteva:
 
     def dali_su_kutevi_ok(self):
         if self.sve_x_kordinate == 1 and self.sve_y_kordinate == 1:
-            self.kutevi_su_ok = True
+            self.kutrvi_su_ok = True
 
 
+# def izracun_tocke_D(a, b, c):
+#     D[0] = c[0] - b[0] + a[0]
+#     D[1] = c[1] - b[1] + a[1]
+#     x_od_abcd.append(D[0])
+#     y_od_abcd.append(D[1])
+#     return D
+def izracun_tocke_D(a, b, c):
+    global D
+    # D = [0, 0]
+    x_od_D = c[0] - b[0] + a[0]
+    # print(x_od_D)
+    y_od_D = c[1] - b[1] + a[1]
+    x_od_abcd.append(D[0])
+    y_od_abcd.append(D[1])
+    D = [x_od_D, y_od_D]
+izracun_tocke_D(A, B, C)
 
 def dali_je_X_unutar_pravokutnika():
     if xx < max(x_od_abcd) and xy < max(y_od_abcd):
@@ -45,8 +60,11 @@ def dijagonala_izracun(a, b, c, d):
     def udaljenost(tocka1, tocka2):
         return math.sqrt((tocka2[0] - tocka1[0]) ** 2 + (tocka2[1] - tocka1[1]) ** 2)
     duljina_stranice1 = udaljenost(a, b)
+    # print(duljina_stranice1)
     duljina_stranice2 = udaljenost(b, c)
+    # print(duljina_stranice2)
     duljina_stranice3 = udaljenost(c, d)
+    # print(duljina_stranice3)
     duljina_stranice4 = udaljenost(d, a)
     if duljina_stranice1 == duljina_stranice2 and duljina_stranice1 == duljina_stranice3 and \
         duljina_stranice2 == duljina_stranice3 and duljina_stranice4 == duljina_stranice1:
@@ -57,46 +75,22 @@ def dijagonala_izracun(a, b, c, d):
         return f"Dijagonala pravokutnika je {diagonala}"
 
 
-def izracun_tocke_D(a, b, c):
-    global D
-    x_od_D = c[0] - b[0] + a[0]
-    y_od_D = c[1] - b[1] + a[1]
-    x_od_abcd.append(x_od_D)
-    y_od_abcd.append(y_od_D)
-    D = [x_od_D, y_od_D]
 
-
-# PROCES:
 
 iste_kordinate_tocaka = dali_su_tocke_razlicite(A, B, C)
-
 if iste_kordinate_tocaka == True:
     print("Molim Vas unesite različite kordinate")
 else:
     pravokutnik = ProvjeraKuteva(x_kordinate_od_abc, y_kordinate_od_abc)
     pravokutnik.dali_su_kutevi_ok()
-    if pravokutnik.kutevi_su_ok == False:
+    if pravokutnik.kutrvi_su_ok == False:
         print("Kordinate ne tvore pravokutnik")
     else:
         izracun_tocke_D(A, B, C)
         print(f"""{ax}, {ay}
 {bx}, {by}
 {cx}, {cy}
-{D[0]}, {D[1]}
+{dx}, {dy}
 {dali_je_X_unutar_pravokutnika()}
         """)
 print(dijagonala_izracun(A, B, C, D))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
